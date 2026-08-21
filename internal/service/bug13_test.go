1f1f134d69d89a623ddc6487b0de92abc5ef74db
jiabo <843051627@qq.com>
2026-08-22 01:52:34 +0800

    refactor: remove test files from branch

--- internal/service/bug13_test.go
+++
@@ -1,3 +0,0 @@
-package service
-import("testing";"ballast-watch/internal/model")
-func TestBug13_RestrictedTankCanBecomeRelease(t *testing.T){if !model.CanTransition(model.StateRestricted,model.StateRelease){t.Fatal("restricted ballast tank cannot become release")}}
