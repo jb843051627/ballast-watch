b9e4546e4e91de46ccd9d47332629136f71fa133
jiabo <843051627@qq.com>
2026-08-22 01:58:25 +0800

    refactor: remove test files from branch

--- internal/service/bug20_test.go
+++
@@ -1,3 +0,0 @@
-package service
-import("context";"testing";"time")
-func TestBug20_SummaryHonorsCancel(t *testing.T){svc:=ballastTestServices(t);ctx,cancel:=context.WithCancel(ballastCtx());cancel();if _,err:=svc.Reports.Summary(ctx,time.Time{},time.Time{});err==nil{t.Fatal("summary ignored cancellation")}}
