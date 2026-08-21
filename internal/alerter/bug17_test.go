ea16f2ff6875c2747c01c2ea2c8cd172e2b9a47c
jiabo <843051627@qq.com>
2026-08-22 01:55:55 +0800

    refactor: remove test files from branch

--- internal/alerter/bug17_test.go
+++
@@ -1,3 +0,0 @@
-package compliance_alerter
-import("testing";"ballast-watch/internal/model")
-func TestBug17_SubmitQueuesOnce(t *testing.T){e:=&Evaluator{queue:make(chan []*model.WaterReading,4)};e.Submit(nil);if n:=len(e.queue);n!=1{t.Fatalf("expected one queued batch, got %d",n)}}
