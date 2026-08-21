c24d10014c51f9b32bae94a460ca127fe058d22e
jiabo <843051627@qq.com>
2026-08-22 01:50:12 +0800

    refactor: remove test files from branch

--- internal/service/bug05_test.go
+++
@@ -1,3 +0,0 @@
-package service
-import("context";"testing")
-func TestBug05_EngineHonorsCancel(t *testing.T){svc:=ballastTestServices(t);ctx,cancel:=context.WithCancel(ballastCtx());cancel();if err:=svc.Engine.Evaluate(ctx,nil);err==nil{t.Fatal("cancelled engine returned nil")}}
