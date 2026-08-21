5d6d6c7f3e9326c8c36296f4b6ae63811be5fdf2
jiabo <843051627@qq.com>
2026-08-22 01:49:25 +0800

    refactor: remove test files from branch

--- internal/service/bug03_test.go
+++
@@ -1,3 +0,0 @@
-package service
-import("sync";"testing";"ballast-watch/internal/store")
-func TestBug03_CacheConcurrentSetGet(t *testing.T){ c:=store.NewCache(); var wg sync.WaitGroup; wg.Add(2); go func(){defer wg.Done();for i:=0;i<20000;i++{c.Set(1,&store.TankSnapshot{BallastTankID:1})}}();go func(){defer wg.Done();for i:=0;i<20000;i++{_ = c.GetAll()}}();wg.Wait() }
