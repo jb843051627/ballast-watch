1f352b30c8385630a180afc65dce4360b92b4d00
jiabo <843051627@qq.com>
2026-08-22 01:56:42 +0800

    refactor: remove test files from branch

--- internal/service/bug18_test.go
+++
@@ -1,3 +0,0 @@
-package service
-import("testing";"ballast-watch/internal/model")
-func TestBug18_DeletedTankCycleReturnsError(t *testing.T){svc:=ballastTestServices(t);v:=mustVessel(t,svc);tank:=mustTank(t,svc,v);c,err:=svc.TreatmentCyclees.Start(ballastCtx(),&model.TreatmentCycleInput{BallastTankID:tank.ID,Name:"cycle",Product:"ballast",Phase:"treat"});if err!=nil{t.Fatal(err)};if _,err=svc.Store.Exec("DELETE FROM tanks WHERE id = ?",tank.ID);err!=nil{t.Fatal(err)};if _,err=svc.TreatmentCyclees.Complete(ballastCtx(),c.ID);err==nil{t.Fatal("deleted tank must return error")}}
