c2eeb59b739e29510471f8dfbfc2d283676b94c4
jiabo <843051627@qq.com>
2026-08-22 02:02:36 +0800

    refactor: remove test files from branch

--- internal/regression/bugs_test.go
+++
@@ -1,6 +0,0 @@
-package regression
-
-import "testing"
-
-// TestBallastRegressionManifest keeps the public regression package discoverable in clean checkouts.
-func TestBallastRegressionManifest(t *testing.T) {}
